cae91e57019d762cd20b232419bf88eea4fce57c
usatecz <usata.veverka@centrum.cz>
2020-04-24 21:24:30 +0200

    Init Commit of UE4 files

---
+++ ArcherArena/Source/ArcherArena/ArcherArenaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcherArenaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHERARENA_API AArcherArenaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
